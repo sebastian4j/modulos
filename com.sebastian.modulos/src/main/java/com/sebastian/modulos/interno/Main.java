0a4522694d123e34c9a8aec0879574e0e4e896f2
Sebastian Avila A <sebastian4j@gmail.com>
2019-03-10 01:40:27 -0300

    algunos problemas en migraciones

--- com.sebastian.modulos/src/main/java/com/sebastian/modulos/interno/Main.java
+++ com.sebastian.modulos/src/main/java/com/sebastian/modulos/interno/Main.java
@@ -1,10 +1,16 @@
 package com.sebastian.modulos.interno;
 
 import com.sebastian.modulos.segundo.exportado.Implementable;
+import java.io.IOException;
+import java.lang.Runtime.Version;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.net.URI;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
@@ -30,7 +36,23 @@ public class Main {
         } catch (ClassCastException e) {
             e.printStackTrace();
         }
-        
+
+        FileSystem fs = FileSystems.getFileSystem(URI.create("jrt:/"));
+        String str = "java/lang/String.class";
+        Path p = fs.getPath("java.base", str);   
+        System.out.println("path: " + p);
+
+        mostrarUrl(ClassLoader.getSystemResource(str));
+        mostrarUrl(ClassLoader.getSystemResource("jdk/internal/jrtfs/ExplodedImage.class"));
+        Version version = Runtime.version();
+        System.out.println("feature: " + version.feature());        
+        System.out.println("interim: " + version.interim());
+        System.out.println("update: " + version.update());
+        System.out.println("patch: " + version.patch());
+    }
+    
+    private static void mostrarUrl(final URL url) throws IOException {
+        System.out.println(null == url ? "~ref url nula~": (url + ": " + url.getContent()));
     }
 
     private void accederClasePackage() throws ClassNotFoundException, NoSuchMethodException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
